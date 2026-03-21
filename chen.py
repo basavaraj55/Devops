32a36be4edf07e571568866bf633d09ea28cd6e5
basavaraj55 <basavaraj.s2345@gmail.com>
2026-03-21 12:36:14 +0530

    add

---
+++ chen.py
@@ -0,0 +1,8 @@
+# Boolean parameter
+is_enabled = True    # or False
+
+# Choice parameter
+color = "red"        # choose: red, green, blue
+
+print("Boolean value:", is_enabled)
+print("Choice selected:", color)
